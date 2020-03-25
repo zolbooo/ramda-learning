8338159fd23d13fdd4a17dff87fbe38c4ac71874
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-25 16:27:00 +0800

    fix: Uncomment tutorial sections

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import 'module-alias/register';
 
 import { invokeTests } from './internal/testing';
 
-// import './getting_started';
-// import './combining_functions';
-// import './partial_application';
-// import './declarative_programming';
-// import './immutability_and_objects';
+import './getting_started';
+import './combining_functions';
+import './partial_application';
+import './declarative_programming';
+import './immutability_and_objects';
 import './immutability_and_arrays';
 
 invokeTests();
